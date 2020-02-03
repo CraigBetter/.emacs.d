094117f3af468a5efc9a353ffa436179397b54c1
U-LPS\cbest <cbest@HAMFWKS23JFCZ1.polysci.com>
2020-02-03 17:38:10 -0400

    added dired+.el

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;;; Load the config
-(org-babel-load-file "/home/craig/.emacs.d/config.org")
+(org-babel-load-file "~/.emacs.d/config.org")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -8,7 +8,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (go-mode which-key use-package telephone-line lua-mode ivy-rich ivy-explorer helm-projectile gruvbox-theme geiser eyebrowse evil-numbers evil-leader evil-commentary evil-collection counsel)))
+    (dired-ranger evil-vimish-fold vimish-fold evil-space go-mode which-key use-package telephone-line lua-mode ivy-rich ivy-explorer helm-projectile gruvbox-theme geiser eyebrowse evil-numbers evil-leader evil-commentary evil-collection counsel)))
  '(which-key-prefix-prefix "+")
  '(which-key-separator " "))
 (custom-set-faces
